105bbda4707f8b1aa25adaadd0d5a1aa41ab9fda
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-12-19 20:13:51 +0100

    Changed so id increments automatically

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class GeografijaDAO {
     private static GeografijaDAO instance = new GeografijaDAO();
-    private static Statement statement;
+    private static Connection connection = null;
 
     public static GeografijaDAO getInstance() {
         return instance;
@@ -24,9 +21,9 @@ public class GeografijaDAO {
             String drzavaCreationQuery = new String(Files.readAllBytes(Paths.get("resources/drzava.sql")), StandardCharsets.UTF_8);
 
             Class.forName("org.sqlite.JDBC");
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:baza.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:baza.db");
 
-            statement = connection.createStatement();
+            Statement statement = connection.createStatement();
             statement.execute(gradCreationQuery);
             statement.execute(drzavaCreationQuery);
 
@@ -37,24 +34,26 @@ public class GeografijaDAO {
     }
 
     private void addInitialData() throws SQLException {
+        Statement statement = connection.createStatement();
+
         if (!statement.executeQuery("SELECT * FROM grad").next()) {
-            statement.executeUpdate("INSERT INTO grad VALUES (1, 'Paris', 2206488, NULL)");
-            statement.executeUpdate("INSERT INTO grad VALUES (2, 'London', 8825000, NULL)");
-            statement.executeUpdate("INSERT INTO grad VALUES (3, 'Beč', 1899055, NULL)");
-            statement.executeUpdate("INSERT INTO grad VALUES (4, 'Manchester', 545500, NULL)");
-            statement.executeUpdate("INSERT INTO grad VALUES (5, 'Graz', 280200, NULL)");
+            statement.executeUpdate("INSERT INTO grad (naziv, broj_stanovnika, drzava) VALUES ('Paris', 2206488, NULL)");
+            statement.executeUpdate("INSERT INTO grad (naziv, broj_stanovnika, drzava) VALUES ('London', 8825000, NULL)");
+            statement.executeUpdate("INSERT INTO grad (naziv, broj_stanovnika, drzava) VALUES ('Beč', 1899055, NULL)");
+            statement.executeUpdate("INSERT INTO grad (naziv, broj_stanovnika, drzava) VALUES ('Manchester', 545500, NULL)");
+            statement.executeUpdate("INSERT INTO grad (naziv, broj_stanovnika, drzava) VALUES ('Graz', 280200, NULL)");
         }
 
         if (!statement.executeQuery("SELECT * FROM drzava").next()) {
-            statement.executeUpdate("INSERT INTO drzava VALUES (1, 'Francuka', 1)");
-            statement.executeUpdate("INSERT INTO drzava VALUES (2, 'Ujedinjeno Kraljevstvo', 2)");
-            statement.executeUpdate("INSERT INTO drzava VALUES (3, 'Austrija', 3)");
+            statement.executeUpdate("INSERT INTO drzava (naziv, glavni_grad) VALUES ('Francuka', 1)");
+            statement.executeUpdate("INSERT INTO drzava (naziv, glavni_grad) VALUES ('Ujedinjeno Kraljevstvo', 2)");
+            statement.executeUpdate("INSERT INTO drzava (naziv, glavni_grad) VALUES ('Austrija', 3)");
         }
 
-        statement.executeUpdate("UPDATE grad SET drzava = 1 WHERE id = 1");
-        statement.executeUpdate("UPDATE grad SET drzava = 2 WHERE id = 2");
-        statement.executeUpdate("UPDATE grad SET drzava = 3 WHERE id = 3");
-        statement.executeUpdate("UPDATE grad SET drzava = 2 WHERE id = 4");
-        statement.executeUpdate("UPDATE grad SET drzava = 3 WHERE id = 5");
+        statement.executeUpdate("UPDATE grad SET drzava = 1 WHERE naziv = 'Paris'");
+        statement.executeUpdate("UPDATE grad SET drzava = 2 WHERE naziv = 'London'");
+        statement.executeUpdate("UPDATE grad SET drzava = 3 WHERE naziv = 'Beč'");
+        statement.executeUpdate("UPDATE grad SET drzava = 2 WHERE naziv = 'Manchester'");
+        statement.executeUpdate("UPDATE grad SET drzava = 3 WHERE naziv = 'Graz'");
     }
 }
