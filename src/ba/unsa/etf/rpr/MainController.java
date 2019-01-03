0b099f81f674d503c6d7cd05e33c9b3a2eb5ac89
Irfan Smajevic <smajevic.irfan@gmail.com>
2019-01-03 02:51:12 +0100

    Being able to find a country by name

--- src/ba/unsa/etf/rpr/MainController.java
+++ src/ba/unsa/etf/rpr/MainController.java
@@ -1,15 +1,19 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -32,8 +36,33 @@ public class MainController implements Initializable {
 
         displayPopup.show();
 
-        GeografijaDAO geografijaDAO = GeografijaDAO.getInstance();
-        System.out.println(geografijaDAO);
-        controller.setItems(geografijaDAO.gradovi());
+        Thread thread = new Thread(() -> {
+            GeografijaDAO geografijaDAO = GeografijaDAO.getInstance();
+            Platform.runLater(() -> {
+                controller.setItems(geografijaDAO.gradovi());
+            });
+        });
+        thread.start();
+    }
+
+    public void findCountry(ActionEvent event) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Unos imena države");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Unesite ime države: ");
+
+        final Optional<String> result = dialog.showAndWait();
+        if (result.isPresent() && !result.get().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Pronađena država");
+            alert.setHeaderText(null);
+
+            Drzava drzava = GeografijaDAO.getInstance().nadjiDrzavu(result.get());
+            alert.setContentText(
+                    String.format("Ime države: %s\nIme glavnog grada: %s", drzava.getNaziv(), drzava.getGlavniGrad().getNaziv())
+            );
+
+            alert.show();
+        }
     }
 }
